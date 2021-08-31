f0a58f1e0ffef7e252ad0dd064a362538f54a8f1
Gilman Huang <gilman.huang213@gmail.com>
2021-08-31 17:03:26 -0400

    method to display expression Closes #16. switch logic btn events Closes #15

--- src/main/java/com/gilmango/calculator/controllers/CalculatorController.java
+++ src/main/java/com/gilmango/calculator/controllers/CalculatorController.java
@@ -8,14 +8,98 @@ import javafx.scene.input.MouseEvent;
 
 public class CalculatorController {
 
-  // labels
+  private final String errorMsg = "ERROR";
+
   @FXML
   private Label expressionLabel;
   @FXML
   private Label resultLabel;
 
+  // appends operand and operator to the math expression displayed
+  private void addToExpression(String input) {
+    String expression = expressionLabel.getText();
+    expressionLabel.setText(
+        input.equals(errorMsg) ? input : expression.concat(input)
+    );
+  }
+
+  // "="
+  // calculate the current displayed expression
+  private void calculateExpression() {
+
+  }
+
+  // "DEL"
+  // delete the previous input entry made by user
+  private void deletePreviousInput() {
+
+  }
+
+  // "HIS"
+  // show past expressions that were entered
+  private void showHistory() {
+
+  }
+
+  // "ANS"
+  // retrieve and display the previous answer calculated
+  private void retrievePreviousAnswer() {
+
+  }
+
+  // "C"
+  // removes the current expression that is displayed
+  private void clearExpression() {
+    expressionLabel.setText("");
+  }
+
+  private void displayError() {
+    expressionLabel.setText(errorMsg);
+  }
 
   public void onMouseClick(MouseEvent mouseEvent) {
+    Button sourceBtn = (Button) mouseEvent.getSource();
 
+    String input = sourceBtn.getText();
+    addToExpression(input);
+
+    switch (input) {
+      // operands
+      case "0":
+      case "1":
+      case "2":
+      case "3":
+      case "4":
+      case "5":
+      case "6":
+      case "7":
+      case "8":
+      case "9":
+
+      // operators
+      case ".":
+      case "+":
+      case "-":
+      case "*":
+      case "/":
+        addToExpression(input);
+        break;
+      case "=":
+        calculateExpression();
+        break;
+
+      // utilities
+      case "DEL":
+        deletePreviousInput();
+      case "ANS":
+        retrievePreviousAnswer();
+      case "HIS":
+        showHistory();
+      case "C":
+        clearExpression();
+      default:
+        displayError();
+        break;
+    }
   }
 }
