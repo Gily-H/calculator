16c2fff6caae9ce8575d49086e2793696409b1f0
Gilman Huang <gilman.huang213@gmail.com>
2021-08-31 23:33:39 -0400

    add util class that checks for precedence. Closes #24

--- src/main/java/com/gilmango/calculator/utilities/StringEvaluator.java
+++ src/main/java/com/gilmango/calculator/utilities/StringEvaluator.java
@@ -1,2 +1,37 @@
-package com.gilmango.calculator.utilities;public class StringEvaluator {
+package com.gilmango.calculator.utilities;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class StringEvaluator {
+
+  public static String convertToPostFix(String expression) {
+    StringBuilder builder = new StringBuilder();
+    Deque<Character> stack = new ArrayDeque<>();
+    char[] letters = expression.toCharArray();
+
+    for (char letter : letters) {
+      if (Character.isDigit(letter) || letter == '.') {
+        builder.append(letter);
+      } else {
+        if (!stack.isEmpty() || !hasHigherPrecedence(letter, stack.peek())) {  // high prec just push on stack
+          builder.append(stack.pop());  // lower prec pop previous operator
+        }
+        stack.push(letter);
+      }
+    }
+
+    return builder.toString();
+  }
+
+
+  public static boolean hasHigherPrecedence(char operator, char operatorToCompare) {
+    boolean isHigher = false;
+
+    if (operatorToCompare == '+' || operatorToCompare == '-')
+      isHigher = operator == '*' || operator == '/';
+
+    return isHigher;
+  }
+
 }
--- src/test/java/StringEvaluatorTest.java
+++ src/test/java/StringEvaluatorTest.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class StringEvaluatorTest {
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.gilmango.calculator.utilities.StringEvaluator;
+import org.junit.jupiter.api.Test;
+
+public class StringEvaluatorTest {
+
+  @Test
+  public void hasHigherPrecedenceTest() {
+    char lower = '+';
+    char lower2 = '-';
+    char higher = '*';
+    char higher2 = '/';
+
+    assertThat(StringEvaluator.hasHigherPrecedence(lower, higher)).isFalse();
+    assertThat(StringEvaluator.hasHigherPrecedence(lower, lower2)).isFalse();
+    assertThat(StringEvaluator.hasHigherPrecedence(higher, lower)).isTrue();
+    assertThat(StringEvaluator.hasHigherPrecedence(higher, higher2)).isFalse();
+  }
 }
