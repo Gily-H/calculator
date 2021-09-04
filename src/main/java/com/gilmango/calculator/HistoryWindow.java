dacc3de784cc8f3df2ff5642d3d8e9153a44eb16
Gilman Huang <gilman.huang213@gmail.com>
2021-09-04 18:12:32 -0400

    history controller updates listview. history window loads fxml. Closes #28. Closes #30

--- src/main/java/com/gilmango/calculator/HistoryWindow.java
+++ src/main/java/com/gilmango/calculator/HistoryWindow.java
@@ -1,2 +1,37 @@
-package com.gilmango.calculator;public class HistoryWindow {
+package com.gilmango.calculator;
+
+import com.gilmango.calculator.controllers.HistoryController;
+import java.io.IOException;
+import java.util.List;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
+public class HistoryWindow {
+
+  public HistoryWindow(List<String> calculations) {
+    try {
+      FXMLLoader loader = new FXMLLoader(getClass().getResource("/history.fxml"));
+      Parent historyRoot = loader.load();
+      historyRoot.getStylesheets()
+          .add(String.valueOf(getClass().getResource("/styles/stylesheet.css")));
+
+      Calculator.getHistoryStage().setScene(new Scene(historyRoot));
+
+      // populate the history window with past calculations
+      HistoryController historyController = loader.getController();
+      historyController.initCalculationHistory(calculations);
+
+      Calculator.getHistoryStage().show();
+
+    } catch (IOException e) {
+      // popup to inform user of an error retrieving history window
+      Alert alert = new Alert(AlertType.ERROR);
+      alert.setTitle("History Window Error");
+      alert.setHeaderText("Unable to show the history");
+      alert.showAndWait();
+    }
+  }
 }
--- src/main/java/com/gilmango/calculator/controllers/HistoryController.java
+++ src/main/java/com/gilmango/calculator/controllers/HistoryController.java
@@ -1,2 +1,15 @@
-package com.gilmango.calculator.controllers;public class HistoryController {
+package com.gilmango.calculator.controllers;
+
+import java.util.List;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+public class HistoryController {
+
+  @FXML
+  private ListView<String> historyList;
+
+  public void initCalculationHistory(List<String> calculations) {
+    calculations.forEach(calculation -> historyList.getItems().add(calculation));
+  }
 }
